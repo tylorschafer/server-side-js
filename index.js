73138d59d38397b03065abfabb4e8024a892b4ab
Tylor Schafer <tylorschafer@gmail.com>
2019-11-14 16:18:08 -0700

    Add papers endpoint

--- index.js
+++ index.js
@@ -12,8 +12,18 @@ app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Publications';
 
 app.get('/', (request, response) => {
-    response.send('Hello, Publications');
+  response.send('Hello, Publications');
 });
+
+app.get('/api/v1/papers', (request, response) => {
+  database('papers').select()
+    .then((papers) => {
+      response.status(200).json(papers);
+    })
+    .catch((error) => {
+      response.status(500).json({ error});
+    });
+})
 
 app.listen(app.get('port'), () => {
     console.log(`${app.locals.title} is running on ${app.get('port')}.`);
